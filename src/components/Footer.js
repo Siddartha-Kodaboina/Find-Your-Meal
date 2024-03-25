6f99d9ac8b2063b258b36f105c3d563a804d45a8
Siddartha Kodaboina <k.siddartha49@gmail.com>
2024-03-25 15:17:30 -0700

    Initial push: Adding all the files for fin your meal app

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div>
+     <footer className="footer">
+      <p>Created by <a className='siddartha' href="https://www.linkedin.com/in/siddartha-kodaboina/" target='_blank'>Siddartha Kodaboina</a> </p>
+      <p>© Find Your Meal 2024</p>
+     
+    </footer>
+    </div>
+    
+  );
+}
+
+export default Footer;
---
+++ src/utils/constant.js
@@ -0,0 +1,33 @@
+//Logo Url
+// export const LOGO_URL="https://drive.google.com/file/d/1CFmycAqPzseaO7zJyE6y-GZj5Wk0FjSO/view";
+// https://drive.google.com/uc?export=view&id=1A2b3C4d5E6f7GhI8J9kL10MnO
+// export const LOGO_URL="https://drive.google.com/uc?export=view&id=1CFmycAqPzseaO7zJyE6y-GZj5Wk0FjSO";
+
+export const LOGO_URL="https://raw.githubusercontent.com/Siddartha-Kodaboina/Find-Your-Meal/master/src/images/dalle_logo.jpeg";
+
+//card information url
+export const IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+
+// Image CDN URL for Restaurant Menu
+export const ITEM_IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+
+// Swiggy API to get Restaurant data with foodfire server
+export const swiggy_api_URL =
+  "https://foodfire.onrender.com/api/restaurants?lat=17.4435&lng=78.3772&page_type=DESKTOP_WEB_LISTING";
+
+// Swiggy API to get Restaurant Menu data with foodfire erver
+export const swiggy_menu_api_URL =
+  "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=17.4435&lng=78.3772&&submitAction=ENTER&restaurantId=";
+
+// shimmer card unit
+export const shimmer_card_unit = 20;
+
+// shimmer Menu card unit
+export const shimmer_menu_card_unit = 4;
+
+// menu items api card type key
+export const MENU_ITEM_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
+export const RESTAURANT_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.Restaurant";
